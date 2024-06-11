a6a09da571bb794035dca5bdf0ef04bf49cdd2e5
achhansrun <107165544+achhansrun@users.noreply.github.com>
2024-06-10 23:49:51 -0800

    Added more animation

--- player.gd
+++ player.gd
@@ -9,9 +9,16 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	#animations
+	if (velocity.x > 1 || velocity.x < -1):
+		$Sprite2D.animation = "run"
+	else:
+		$Sprite2D.animation = "idle"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		$Sprite2D.animation = "jump"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
