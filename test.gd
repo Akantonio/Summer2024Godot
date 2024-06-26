159343563badd66a3ac4eba9d9faf604db6f4978
achhansrun <107165544+achhansrun@users.noreply.github.com>
2024-06-26 00:57:03 -0800

    Added gui for health system, and methods to update it. Also created a signal from player to update the gui based on the max number of health the player has and what their current health is

---
+++ test.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var heartsContainer = $CanvasLayer/heartsContainer
+@onready var player = $Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	heartsContainer.setMaxHearts(player.maxHealth)
+	heartsContainer.updateHearts(1)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
